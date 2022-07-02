014d78156abacc70d7ab26d4a05c995568d15719
ruzio <carlos.9942@gmail.com>
2022-07-02 19:11:22 -0400

    Endpoint /usuarios - listado de usuarios [GET]

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,26 @@ class UserController extends Controller
 {
 
     public function getUsers(){
-        dd('Obtener');
+        // dd('Obtener');
+        $req = Request();
+        // print_r($req);
+        $cliente = new Client();
+        $respuesta = $cliente->request('GET', 'https://gorest.co.in/public/v2/users');
+        $contenido = $respuesta->getBody()->getContents();
+        // print_r(gettype(($contenido)));
+        $datosJson = json_decode($contenido);
+        // print_r(gettype($datosJson));
+        $datos = collect($datosJson)->map(function ($item) {
+            return [
+                'id'        => $item->id,
+                'nombre'    => $item->name,
+                'email'     => $item->email,
+                'genero'    => $item->gender,
+                'activo'    => $item->status === 'active' ? true : false
+            ];
+        });
+        // dd(gettype($datos));
+        return $datos;
     }
 
     public function addUser(){
